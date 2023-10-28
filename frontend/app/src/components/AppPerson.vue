210da9a0b2a8a283a4d63d6c4be35f1ad2837a19
Nikita Lysenkov <Nikitalysenkov8@gmail.com>
2023-10-28 11:30:00 +0300

    #

---
+++ frontend/app/src/components/AppPerson.vue
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import AppRegister from '../components/AppRegister.vue'
+import AppLogin from '../components/AppLogin.vue'
+import ListOrganization from '../components/ListOrganization.vue'
+import AppMenu from '../components/AppMenu.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/registration',
+      name: 'register',
+      component: AppRegister,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: AppLogin,
+    },
+    {
+      path: '/list',
+      name: 'listorganization',
+      component: ListOrganization,
+    },
+    {
+      path: '/menu',
+      name: 'menu',
+      component: AppMenu,
+      alias: "/"
+    },
+  ]
+})
+
+export default router
